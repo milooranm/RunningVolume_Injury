38e894b52a97f91131b0e34e4400d555afba27b1
Milo <milomoran123@gmail.com>
2025-07-20 15:20:26 +0100

    working basic FastAPI file
    runs, produces the image when I make the post request for it, boom. to be iterated on significantly

--- MVP2 notebooks/test_fastapi.py
+++ MVP2 notebooks/test_fastapi.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI, Response
 from fastapi.responses import StreamingResponse
+from fastapi.responses import HTMLResponse
+
 import pickle
-import io
-import os
+
+
 from PIL import Image
 from io import BytesIO
 
 # import the model
-with open('.\models\mvp2best_logistic_model.pkl', 'rb') as file:
+with open('..\models\mvp2best_logistic_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
 # load test image
@@ -45,3 +47,31 @@ async def predict_and_visualize(X:int = 2):
     
     except Exception as e:
         return {"error": str(e)}, 500
+'''   
+@app.get("/view/", response_class=HTMLResponse)
+async def view():
+    return """
+    <html>
+        <body>
+            <h2>Injury Risk Prediction</h2>
+            <div>
+                <input type="number" id="x-value" value="2">
+                <button onclick="makePrediction()">Predict</button>
+            </div>
+            <img src="" id="prediction-image">
+            
+            <script>
+                async function makePrediction() {
+                    const xValue = document.getElementById('x-value').value;
+                    const response = await fetch('/predict_and_visualize/?X=' + xValue, {
+                        method: 'POST'
+                    });
+                    const blob = await response.blob();
+                    const img = document.getElementById('prediction-image');
+                    img.src = URL.createObjectURL(blob);
+                }
+            </script>
+        </body>
+    </html>
+    """
+'''   
