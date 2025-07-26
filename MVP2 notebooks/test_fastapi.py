9a348acf5557587e85ce7909db1e687c3ac66fdf
Milo <milomoran123@gmail.com>
2025-07-26 17:55:56 +0100

    making the endpoiint interactive in prep for the full tool

--- MVP2 notebooks/test_fastapi.py
+++ MVP2 notebooks/test_fastapi.py
@@ -13,12 +13,15 @@ with open('..\models\mvp2best_logistic_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
 # load test image
-def load_image():
-    img = Image.open("injury.png")
-    buffer = BytesIO()
-    img.save(buffer, format='PNG')
-    buffer.seek(0)
-    return buffer
+def load_image(username: str, password: str):
+    if username and password:
+        img = Image.open("injury.png")
+        buffer = BytesIO()
+        img.save(buffer, format='PNG')
+        buffer.seek(0)
+        return buffer
+    else:
+        reutrn None
 
 
 app = FastAPI(
@@ -30,23 +33,48 @@ app = FastAPI(
 async def root():
     return {"message": "test for injury_risk_pred is running!"}
 
+# Route: HTML form served directly from Python
+@app.get("/", response_class=HTMLResponse)
+def login_form():
+    html_content = """
+    <html>
+        <head><title>Login</title></head>
+        <body>
+            <h2>Please login</h2>
+            <form action="/login" method="post">
+                <label>Username:</label>
+                <input type="text" name="username"><br/>
+                <label>Password:</label>
+                <input type="password" name="password"><br/>
+                <button type="submit">Login</button>
+            </form>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content)
+
+
+
 @app.post("/predict_and_visualize/")
-async def predict_and_visualize(X:int = 2):
+async def predict_and_visualize(username:str = Form(...), password:str = Form(...)):
     """
     perform spoof calculation and return an image
     """
     try:
         
         # output the number squared
-        X = X * 2
-
+        if username and password:
         # handle the image
-        img = load_image()
+            img = load_image()
             
-        return StreamingResponse(img, media_type="image/png")
+            return StreamingResponse(img, media_type="image/png")
+        else:
+            return {"error": "Invalid username or password"}, 401
     
     except Exception as e:
         return {"error": str(e)}, 500
+
+
 '''   
 @app.get("/view/", response_class=HTMLResponse)
 async def view():
