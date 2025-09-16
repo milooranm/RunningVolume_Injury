b89cbb4c719cb5a62670c9a7a18d74509e32eddf
milooranm <milomoran123@gmail.com>
2025-09-16 20:48:52 +0100

    for a bit more visibility put days = 120

--- Render/apicall_input.py
+++ Render/apicall_input.py
@@ -114,7 +114,7 @@ def main_api_call(email=None, password=None):
 
     
     end_date = datetime.date.today()
-    start_date = end_date - datetime.timedelta(days=150)
+    start_date = end_date - datetime.timedelta(days=120)
 
     if not email or not password:
         print("Email and password are required for authentication.")
