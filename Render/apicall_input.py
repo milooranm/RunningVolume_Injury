82b482a25238a52b6172255fca97e55b97995fdf
milooranm <milomoran123@gmail.com>
2025-12-02 13:46:04 +0000

    updated/reviewed apicall_input.py

--- Render/apicall_input.py
+++ Render/apicall_input.py
@@ -2,6 +2,7 @@ import datetime
 import logging
 import os
 import csv
+import sys
 from getpass import getpass
 import requests
 from io import BytesIO
@@ -20,7 +21,7 @@ logger = logging.getLogger(__name__)
 
 
 
-def init_api(email, password): #remove tokenstore
+def init_api(email, password):
     """Initialize Garmin API with your credentials."""
     try:
         garmin = Garmin(email, password)
@@ -33,13 +34,13 @@ def init_api(email, password): #remove tokenstore
     return garmin
 
 
-
+# Deprecated function prior to in-memory handling
 def get_activity_files(api, start_date, end_date, output_dir="./"):
     """Downloads activity files within a date range."""
-    activitytype=""
+    
     try:
         activities = api.get_activities_by_date(
-            start_date.isoformat(), end_date.isoformat(), activitytype
+            start_date.isoformat(), end_date.isoformat()
         )
 
         for activity in activities:
@@ -71,12 +72,11 @@ def get_activity_dataframes(api, start_date, end_date):
     Retrieves activity data within a date range and returns a list of dictionaries,
     each containing the filename and its corresponding DataFrame.
     """
-    activitytype = ""
     activity_data = []
 
     try:
         activities = api.get_activities_by_date(
-            start_date.isoformat(), end_date.isoformat(), activitytype
+            start_date.isoformat(), end_date.isoformat()
         )
 
         for activity in activities:
@@ -103,6 +103,7 @@ def get_activity_dataframes(api, start_date, end_date):
         GarminConnectTooManyRequestsError,
         requests.exceptions.HTTPError,
     ) as err:
+        logger.error(err)
         print(f"Error downloading activities: {err}")
 
     return activity_data
@@ -112,28 +113,33 @@ def main_api_call(email=None, password=None):
     """Main function to download Garmin Connect activities."""
     print("Garmin Connect API - Activity Downloader")
 
-    
     end_date = datetime.date.today()
     start_date = end_date - datetime.timedelta(days=120)
 
     if not email or not password:
+        logger.error("Email and password are required for authentication.")
         print("Email and password are required for authentication.")
         return 0, 0, 0
 
     api = init_api(email, password)
 
     if not api:
+        logger.error("Failed to initialize Garmin API. Exiting.")
         print("Failed to initialize Garmin API. Exiting.")
         return None, None, None
 
-    if not end_date:
-        end_date = datetime.datetime.now()
-
-    name_and_data =get_activity_dataframes(api, start_date, end_date)
+    name_and_data = get_activity_dataframes(api, start_date, end_date)
 
     return start_date, end_date, name_and_data
 
 if __name__ == "__main__":
 
+    if len(sys.argv) != 3:
+        print('correct usage requires email and password space separated')
+        sys.exit(1)
+        
+    email = sys.argv[1]
+    password = sys.argv[2]
 
-    start_date ,end_date, name_and_data = main_api_call()
+
+    start_date ,end_date, name_and_data = main_api_call(email, password)
